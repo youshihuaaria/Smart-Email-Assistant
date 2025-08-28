40bedab168e08a6b8bd18a77f495b287862728de
Aria You <62320933+youshihuaaria@users.noreply.github.com>
2025-08-27 22:31:26 -0700

    Initial commit

---
+++ lambda/src/main/java/com/myorg/lambda/App.java
@@ -0,0 +1,13 @@
+package com.myorg.lambda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
